6b94b878ff975439d167076c0476b44385ff29fe
Tomas Jansson <tomas.jansson@gmail.com>
2015-06-23 21:12:21 +0200

    docker env setup

--- environment/docker/provision.sh
+++ environment/docker/provision.sh
@@ -2,4 +2,6 @@
 
 eval "$(docker-machine env)"
 
-docker run -d --name=elasticsearch -p 9200:9200 -p 9300:9300 elasticsearch
+docker run -d --name=elasticsearch -p 9200:9200 -p 9300:9300 mastoj/elastic-marvel
+docker run -d --name=neo4j -p 7474:7474 mastoj/neo4j
+docker run -d --name=eventstore -p 2113:2113 -p 1113:1113 mastoj/eventstore
